23e6aa34fa86e9c0fb6e3df3cfeee7f0c2ac8810
tiko2015 <tiko2015@gmail.com>
2015-01-08 16:51:09 -0300

    add grunt browser sync and remove express server

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,134 +5,128 @@
 'use strict';
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-  server: {
-     port: 3000,
-     base: './dist'
-  },
-  stylus: {
-    compile: {
-      options: {
-        compress: true,
-        paths: ['stylus'],
-        urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
-        use: [
-          require('nib'),
-          require('jeet'),
-          require('rupture')
-        ]
-      },
-      import: [
-        'nib','jeet'
-      ],
-      files: [ {
-        cwd: "app/assets/stylus",
-        src: "*.styl",
-        dest: "dist/assets/css/",
-        expand: true,
-        ext: ".css"
-      } ]
-    }
-  },
+    grunt.initConfig({
+        server: {
+            port: 3000,
+            base: './dist'
+        },
+        stylus: {
+            compile: {
+                options: {
+                    compress: true,
+                    paths: ['stylus'],
+                    urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
+                    use: [
+                        require('nib'),
+                        require('jeet'),
+                        require('rupture')
+                    ]
+                },
+                import: [
+                    'nib', 'jeet'
+                ],
+                files: [{
+                    cwd: "app/assets/stylus",
+                    src: "*.styl",
+                    dest: "dist/assets/css",
+                    expand: true,
+                    ext: ".css"
+                }]
+            }
+        },
+        browserSync: {
+            bsFiles: {
+                src: [
+                    'dist/*.html',
+                    'dist/**/*.html',
+                    'dist/assets/**/*.css',
+                ]
+            },
+            options: {
+                watchTask: true,
+                server: {
+                    baseDir: "./dist"
+                }
+            }
+        },
+        jade: {
+            compile: {
+                options: {
+                    client: false,
+                    pretty: true,
+                    data: function(dest, src) {
+                        var src = String(src)
+                            .replace("app/views", "app/models")
+                            .replace(".jade", ".json");
+                        var global = grunt.file.readJSON("app/models/_global.json")
 
-  jade: {
-    compile: {
-      options: {
-        client: false,
-        pretty: true,
-        data: function(dest, src) {
-          var src = String(src)
-            .replace("app/views", "app/models")
-            .replace(".jade", ".json");
-          var global = grunt.file.readJSON("app/models/_global.json")
+                        if (grunt.file.exists(src)) {
+                            var data = grunt.file.readJSON(src);
+                            data.global = global;
+                            return data;
+                        }
+                        return {
+                            global: global
+                        };
+                    }
+                },
+                files: [{
+                    cwd: "app/views",
+                    src: ["**/*.jade", "!_layouts/**", "!**/_*.jade"],
+                    dest: "dist",
+                    expand: true,
+                    ext: ".html"
+                }]
+            }
+        },
 
-          if(grunt.file.exists(src)) {
-            console.log(src);
-            var data = grunt.file.readJSON(src);
-            data.global = global;
-            return data; 
-          }
-          return { global: global };
-        }
-      },
-      files: [ {
-        cwd: "app/views",
-        src: ["**/*.jade","!_layouts/**","!**/_*.jade"],
-        dest: "dist",
-        expand: true,
-        ext: ".html"
-      } ]
-    }
-  },
+        watch: {
+            css: {
+                files: ['app/assets/stylus/*.styl', 'app/assets/stylus/**/*.styl'],
+                tasks: ['stylus'],
+                
+            },
+            views: {
+                files: ['app/views/*.jade', 'app/views/**/*.jade', 'app/models/**/*.json', 'app/models/*.json'],
+                tasks: ['jade'],
+                
+            },
+            js: {
+                files: ['app/assets/js/*.js', 'app/assets/js/**/*.js'],
+                tasks: ['uglify'],
+                
+            }
+        },
 
-  watch: {
-    css: {
-      files: ['app/assets/stylus/*.styl', 'app/assets/stylus/**/*.styl'],
-      tasks: ['stylus'],
-      options: {
-        debounceDelay: 250
-      }
-    },
-    views: {
-      files: ['app/views/*.jade', 'app/views/**/*.jade', 'app/models/**/*.json', 'app/models/*.json'],
-      tasks: ['jade'],
-      options: {
-        debounceDelay: 250
-      }
-    },
-    js: {
-      files: ['app/assets/js/*.js', 'app/assets/js/**/*.js'],
-      tasks: ['uglify'],
-      options: {
-        debounceDelay: 250
-      }
-    }
-  },
+        clean: {
+            dist: ["dist/assets/css/*.css", "dist/assets/js/*.js"]
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'dist/assets/js/bootstrap.min.js': [
+                        'app/assets/js/lib/*.js',
+                        'app/assets/js/lib/**/*.js'
+                    ],
+                    'dist/assets/js/script.js': [
+                        'app/assets/js/script.js'
+                    ]
+                }
+            }
+        },
 
-  clean: {
-    dist: ["dist/assets/css/*.css", "dist/assets/js/*.js"]
-  },
-  uglify: {
-    dist: {
-      files: {
-        'dist/assets/js/bootstrap.min.js': [
-            'app/assets/js/bootstrap/transition.js',
-            'app/assets/js/bootstrap/alert.js',
-            'app/assets/js/bootstrap/button.js',
-            'app/assets/js/bootstrap/carousel.js',
-            'app/assets/js/bootstrap/collapse.js',
-            'app/assets/js/bootstrap/dropdown.js',
-            'app/assets/js/bootstrap/modal.js',
-            'app/assets/js/bootstrap/tooltip.js',
-            'app/assets/js/bootstrap/popover.js',
-            'app/assets/js/bootstrap/scrollspy.js',
-            'app/assets/js/bootstrap/tab.js',
-            'app/assets/js/bootstrap/affix.js'
-        ],
-        'dist/assets/js/script.js': [
-          'app/assets/js/script.js'
-        ]
-      }
+    });
 
-    }
-  },
+    // Load plugins here
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks("grunt-contrib-jade");
+    grunt.loadNpmTasks('grunt-browser-sync');
 
-  });
+    grunt.registerTask('default', ["browserSync", "watch"]);
 
-  // Load plugins here
-  grunt.loadNpmTasks('grunt-contrib-stylus');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks("grunt-contrib-jade");
+    grunt.registerTask('build', ['jade', 'clean', 'stylus', 'uglify']);
 
-  grunt.registerTask('build', ['jade','clean','stylus', 'uglify']);
-  grunt.registerTask('server', 'Start a custom web server', function() {
-      grunt.log.writeln('Started web server on port ' + grunt.config.get('server.port') );
-      var base = grunt.config.get('server.base');
-      var port = grunt.config.get('server.port')
-      require('./app.js')
-        .use("/", express.static(base))
-        .listen(port);
-  });
 };
--- server.js
+++
@@ -1,6 +0,0 @@
-express = require('express');
-app = module.exports = express();
-var port = process.env.PORT || 3000;
-app.use("/", express.static('./dist/'));
-app.listen(port);
-console.log('Server running in port:' + port );
